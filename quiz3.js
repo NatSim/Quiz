bec178570df29ae94f694d678c99cd341b6899bf
NatSim <natsimbig@gmail.com>
2020-10-12 16:56:46 +0100

    Adding functions to display all objects in array

--- quiz3.js
+++ quiz3.js
@@ -1,43 +1,58 @@
 import quizQuestions from "./quiz_questions.js";
 console.log(quizQuestions);
 
-//Quiz Title- append to root
-let quizTitle = document.createElement("h1");
-let titleParent = document.getElementById("quiz");
-titleParent.appendChild(quizTitle);
-quizTitle.innerHTML = "Music Quiz";
-titleParent.insertBefore(quizTitle, titleParent.getElementsByTagName("div")[0]);
-
-//Question container elements
-const container = document.createElement("div"); //loop from here
-const question = document.createElement("p");
-const questionText = document.createTextNode(
-  "Who sang the song, 'The Fresh Prince of JA'"
-);
-
-question.appendChild(questionText);
-container.appendChild(question);
-
-const quiz = document.getElementById("quiz");
-quiz.appendChild(container);
-
-//Linking Array items
-const choices = quizQuestions[0].choices;
-
-const answerContainer = document.createElement("div");
-//For Looping through array 
-for (let i = 0; i < choices.length; i++) {
-  const answer = document.createElement("p");
-  const input = document.createElement("input");
-  input.type = "radio";
-  input.name = "question1";
-
-  answer.appendChild(input); //(appended item)
-
-  const answerText = document.createTextNode(choices[i]);
-  answer.appendChild(answerText);
-
-  answerContainer.appendChild(answer);
+function createTitle() {
+  //Quiz Title- append to root
+  let quizTitle = document.createElement("h1");
+  let titleParent = document.getElementById("quiz");
+  titleParent.appendChild(quizTitle);
+  quizTitle.innerHTML = "Music Quiz";
+  titleParent.insertBefore(
+    quizTitle,
+    titleParent.getElementsByTagName("div")[0]
+  );
 }
 
-quiz.appendChild(answerContainer);
+function createChoices(choices, index) {
+  //Linking Array items
+  // const choices = quizQuestions[0].choices;
+
+  const answerContainer = document.createElement("div");
+  //For Looping through answers
+  for (let i = 0; i < choices.length; i++) {
+    const answer = document.createElement("p");
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.name = "question" + index;
+
+    answer.appendChild(input); //(appended item)
+
+    const answerText = document.createTextNode(choices[i]);
+    answer.appendChild(answerText);
+
+    answerContainer.appendChild(answer);
+  }
+
+  quiz.appendChild(answerContainer);
+}
+
+function createQuiz() {
+  for (let i = 0; i < quizQuestions.length; i++) {
+    const obj = quizQuestions[i];
+    //Question container elements
+    const container = document.createElement("div"); //loop from here
+    const question = document.createElement("p");
+    const questionText = document.createTextNode(obj.question.toUpperCase());
+
+    // Append Questions
+    question.appendChild(questionText);
+    container.appendChild(question);
+
+    // const quiz              = document.getElementById("quiz"); already got id earlier?
+    quiz.appendChild(container);
+    // console.log(obj.question);
+    createChoices(obj.choices, i);
+  }
+}
+createTitle();
+createQuiz();
