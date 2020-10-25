5879b7977c4f07843ea67a690b08e6a3d3ed606c
NatSim <natsimbig@gmail.com>
2020-10-25 18:12:55 +0000

    Added a submit button, making a correct answer choosen function

--- quiz3.js
+++ quiz3.js
@@ -3,10 +3,10 @@ console.log(quizQuestions);
 
 function createTitle() {
   //Quiz Title- append to root
-  let quizTitle = document.createElement("h1");
-  let titleParent = document.getElementById("quiz");
+  let quizTitle        = document.createElement("h1");
+  let titleParent      = document.getElementById("quiz");
   titleParent.appendChild(quizTitle);
-  quizTitle.innerHTML = "Music Quiz";
+  quizTitle.innerHTML  = "Music Quiz";
   titleParent.insertBefore(
     quizTitle,
     titleParent.getElementsByTagName("div")[0]
@@ -21,9 +21,9 @@ function createChoices(choices, index) {
   //For Looping through answers
   for (let i = 0; i < choices.length; i++) {
     const answer = document.createElement("p");
-    const input = document.createElement("input");
-    input.type = "radio";
-    input.name = "question" + index;
+    const input  = document.createElement("input");
+    input.type   = "radio";
+    input.name   = "question" + index;
 
     const quizQuestionMark = answer.appendChild(input); //(appended item)
 
@@ -38,29 +38,69 @@ function createChoices(choices, index) {
 function createQuiz() {
   for (let i = 0; i < quizQuestions.length; i++) {
     const obj = quizQuestions[i];
-    //Question container elements
-    const container = document.createElement("div"); //loop from here
-    const question = document.createElement("p");
+    //Question Container Elements
+    const container    = document.createElement("div"); //loop from here
+    const question     = document.createElement("p");
     const questionText = document.createTextNode(obj.question.toUpperCase());
-    const qMarks   = document.createTextNode("?");
+    const qMarks       = document.createTextNode("?");
+    
 
-    // Append Questions
+    // Append Question Elements
     question.appendChild(questionText);
     container.appendChild(question);
     question.appendChild(qMarks);
 
     // const quiz              = document.getElementById("quiz"); already got id earlier?
     quiz.appendChild(container);
-    // console.log(obj.question);
+    
     createChoices(obj.choices, i); //each creatQuiz iteration= createChoices is also called
 
-    // addQMarks();
+    
   }
+
+//Create Submit button
+const submitButton = document.createElement("button");
+const buttonContent  = document.createTextNode(" Submit ");
+
+submitButton.appendChild(buttonContent);
+
+const getRoot = document.getElementById("result");
+
+getRoot.appendChild(submitButton)
+
+
+//Picked Input Function
+
+function getCorrectAnswer() {
+  //Question& Correct Answers
+  let q1 = document.quizQuestions[0].choices[1].value;
+  let q2 = document.quizQuestions[1].choices[0].value
+  let q3 = document.quizQuestions[2].choices[0].value
+  let q4 = document.quizQuestions[3].choices[2].value
+  let q5 = document.quizQuestions[2].choices[3].value
+
+  let count       = 0;
+  let picked;
+
+  if (picked === q1 ) {
+    console.log("Correct"); true && count++;
+  } else{
+    console.log("Wrong"); false;
+  }
+
 }
 
+console.log(quizQuestions[0].answer)
+console.log(quizQuestions[0].choices[1])
 
-console.log("hello");
+
+
+
+
+
+
+}
 
 createTitle();
 createQuiz();
-
+// getCorrectAnswer();
