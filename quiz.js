9080726315e2c71e8e45853d59887f78ed0b6ad9
NatSim <natsimbig@gmail.com>
2020-11-05 17:41:34 +0000

    adding function to calculate&present score at end of quiz

--- quiz.js
+++ quiz.js
@@ -17,14 +17,21 @@ let totalScore = 0;
 
 function radioEventCreator(radio){
   radio.onchange = (e) => {
-    // @todo Just to make totalScore reflect reality
+    // totalScore reflect reality
     
-    totalScore += e.target.value ++  ;
+    totalScore += parseInt(e.target.value, 10);
   };
 
 }
-
+/**
+ * createChoices
+ * 
+ * this creates choices for the quiz
+ * @params {string[]} choices - The answer choices
+ * @params {number} index - This is the question index
+ */
 function createChoices(choices,index) {
+  const DEFAULT_VALUE = 0;
   //Linking Array items
   const answerContainer = document.createElement("div");
   //For Looping through answers
@@ -32,17 +39,18 @@ function createChoices(choices,index) {
     const answer = document.createElement("p");
     const input  = document.createElement("input");
     input.type   = "radio";
-    input.name   = "question" ;
+    input.name   = "question" + index ;
+    input.value = DEFAULT_VALUE;
     radioEventCreator(input);
 
-    input.id = "choices" ;
-    if ( i == quizQuestions[index].answer) {
+    // Statement to set value to 1 for correct answers
+    if ( i === quizQuestions[index].answer) {
       input.value = 1;
-    } else {
-      input.value = 0 && false;
     }
-
-    // console.log(input.id)
+    
+    // else {
+    //   input.value = 0;
+    // }
 
     answer.appendChild(input); //(appended input element)
 
@@ -56,67 +64,78 @@ function createChoices(choices,index) {
 
 function createQuiz() {
   for (let i = 0; i < quizQuestions.length; i++) {
-    const obj = quizQuestions[i];
+    const questionObj = quizQuestions[i];
     //Question Container Elements
     const container    = document.createElement("div"); //loop from here
     const question     = document.createElement("label");
-    const questionText = document.createTextNode(obj.question.toUpperCase());
+    const questionText = document.createTextNode(questionObj.question.toUpperCase());
     const qMarks       = document.createTextNode("?");
-    
+  
 
     // Append Question Elements
     question.appendChild(questionText);
-    container.appendChild(question);
     question.appendChild(qMarks);
+    container.appendChild(question);
 
-    // const quiz              = document.getElementById("quiz"); already got id earlier?
+    
     quiz.appendChild(container);
     
-    createChoices(obj.choices, i); //each creatQuiz iteration= createChoices is also called    
+    createChoices(questionObj.choices, i); //each creatQuiz iteration= createChoices is also called
   }
 
   //Create Submit button
   const submitButton = document.createElement("button");
   const buttonContent  = document.createTextNode(" Submit ");
   submitButton.type   = "button";
-  submitButton.onclick="_blank" 
   submitButton.appendChild(buttonContent);
 
 
-  // Onclick execute function ones
-let exitSubmit = (e) => { 
+  // Onclick execute function onces only
+  let exitSubmit = (e) => { 
+    submitButton.onclick = false;
+  }
+
+  //User Results Arrow Function
+  const userResults = () => {   
+    let resultDiv = document.createElement("div");
+    let result = document.createElement("p");
+    result.className = "result";
+    result.textContent = "You're score is:" +  totalScore + " out of " + quizQuestions.length;
+    resultDiv.appendChild(result);
+    return resultDiv;
+    
+  }
+  console.log();
+
+  function highlightSelectedChoices(questionNumber){
+    const choices = document.getElementsByName("question0");
+
+    for(let i = 0; i < choices.length; i++) {
+      const currentChoice = choices[i];
+
+      if(currentChoice.checked) {
+        // change 0 the use the current question
+        console.log(i, quizQuestions[0].answer);
+        if(i === quizQuestions[0].answer) {
+
+          // set styling to the correct color
+          console.log('input turns green');
+        } else {
+          console.log('input turns red')
+        }
+
+
+        break;
+      }
+    }
+  }
+ 
   
-  submitButton.onclick = exitSubmit && false;
-  
-}
-//User Results Arrow Function
-const userResults = () => {   
-  let resultDiv = document.createElement("div");
-  let result = document.createElement("p");
-  result.className = "result";
-  result.textContent = "You're score is:" +  totalScore + " out of " + quizQuestions.length;
-  resultDiv.appendChild(result);
-  return resultDiv;
-  
-}
-
-//Function to reveal which answers correct & incorrect
-// const feedbackColours = () => {
-
-//   if choices   
-// } 
-
-
-
-
-
-
-
-//Button onclick Function(executes Results function)
+  //Button onclick Function(executes Results function)
   let form = document.getElementById("quiz");
   submitButton.onclick = (e) => {
-      
     form.append(userResults());
+    highlightSelectedChoices();
     exitSubmit(); 
     
   };
